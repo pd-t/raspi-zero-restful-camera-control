bec683e0b12af5e8df1f6b3478df297f23ef7cfb
Thomas Fraunholz <tf@wogra.com>
2022-04-23 09:43:21 +0200

    fix wrong path in install script

--- init.sh
+++ init.sh
@@ -12,7 +12,7 @@
 
 case "$1" in
  start)
- source config.cfg
+ source /opt/camera-control/config.cfg
  mkdir -p "$CAMERA_DATA_PATH"
  WEB_CONCURRENCY=1 gunicorn --worker-class uvicorn.workers.UvicornWorker --config /opt/camera-control/gunicorn_conf.py --chdir /opt/camera-control src.main:app
  ;;
